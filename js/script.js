e124000caa57a7b8b3a8b14b2bcf79810257fdc6
bartnewk29@gmail.com <bartnewk29@gmail.com>
2020-05-30 19:30:37 +0200

    Zmiany-Matma

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+let computerMove = `kamień`;
+let playerMove = "papier" ;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+// Argument wyświetlenia Tekstu chyba
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
