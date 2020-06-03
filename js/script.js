7018455087371d0a9f89202f89e1a9f090c247f2
bartnewk29@gmail.com <bartnewk29@gmail.com>
2020-06-03 15:41:52 +0200

    If elsse

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 
 let computerMove = `kamień`;
+if(2 > 3){
+	printMessage('Niesamowite! Jeden jest większe niż dwa!!!');
+} else if (1 == 3) {
+	printMessage('Dziwne – jeden jest równe dwa?!');
+} else {
+	printMessage('Wszystko po staremu, jeden jest mniejsze niż dwa.');
+}
 let playerMove = "papier" ;
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
@@ -16,3 +23,7 @@ printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
 let roundNumber = Math.floor(calculation);
 
 printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
