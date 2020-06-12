a4860608cb4b8cf960e338f18dda9de5b9b54a51
bartnewk29@gmail.com <bartnewk29@gmail.com>
2020-06-12 13:46:46 +0200

    HTML,CSS,JS-Ready in 50%

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+console.log("Action");
 function playGame(playerMove) {
   clearMessages();
 
@@ -36,4 +37,3 @@ function displayResult(argComputerMove, argPlayerMove) {
   }
 }
 
-console.log("What don't work");
